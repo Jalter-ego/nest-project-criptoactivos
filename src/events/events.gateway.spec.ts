c16d063761244f0e5bf5806419dacc9c47523556
jalter-ego <jhademels123@gmail.com>
2025-10-30 14:06:53 -0400

    fix: fixed test to deploy

--- src/events/events.gateway.spec.ts
+++ src/events/events.gateway.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsGateway } from './events.gateway';
+import { CoinbaseService } from './coinbase/coinbase.service';
 
 describe('EventsGateway', () => {
   let gateway: EventsGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventsGateway],
+      providers: [
+        EventsGateway,
+        {
+          provide: CoinbaseService,
+          useValue: {
+            handleEvent: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     gateway = module.get<EventsGateway>(EventsGateway);
