b26ee31c7ea94107c37920bb32edc25e464c9ca6
jalter-ego <jhademels123@gmail.com>
2025-11-16 19:04:08 -0400

    feat: update fastApi endpoint to production URL and comment out local development URL

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,3 @@
-export const fastApi = 'https://fastapi-project-criptoactivos.onrender.com'
-//export const fastApi = 'http://127.0.0.1:8000'
+export const fastApi =
+  'https://fastapi-project-criptoactivos-production.up.railway.app';
+//export const fastApi = 'http://127.0.0.1:8000/api/v1'
