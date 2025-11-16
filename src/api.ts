9d3c366ab13376d8d63aa56752830dea1134168b
jalter-ego <jhademels123@gmail.com>
2025-11-16 19:45:51 -0400

    fix: fixed deploy

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,3 @@
 export const fastApi =
-  'https://fastapi-project-criptoactivos-production.up.railway.app';
+  'https://fastapi-project-criptoactivos-production.up.railway.app/api/v1';
 //export const fastApi = 'http://127.0.0.1:8000/api/v1'
