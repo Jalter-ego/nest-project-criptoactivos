625b3018182136bee1da8f3eecfd6828564398b2
jalter-ego <jhademels123@gmail.com>
2025-11-19 21:20:56 -0400

    feat: update fastApi endpoint to new AWS App Runner URL

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-export const fastApi = 'https://fastapi-project-criptoactivos-production.up.railway.app/api/v1';
+export const fastApi = 'https://3d6xe7zu33.us-east-1.awsapprunner.com/api/v1';
 //export const fastApi = 'http://127.0.0.1:8000/api/v1'
